ea4e9b1970c15355718c2d71d187379209d0e45d
John Soeder <jsoeder20@gmail.com>
2023-12-11 23:56:53 -0500

    Fixed bug in longest route calculation

--- score_game.py
+++ score_game.py
@@ -11,37 +11,38 @@ def train_points(df, scores):
     for key in scores.keys():
         scores[key] += df[df['color']==key]['points'].sum()
 
-def single_longest_route(color_df, visited, city, curr_path_length, longest_path):
-    if city in visited:
-        return
-    
-    visited.add(city)
-    
+def single_longest_route(color_df, visited, city, curr_path_length, longest_path):    
     for index, row in color_df.iterrows():
-        if row['location1'] == city and row['location2'] not in visited:
-            single_longest_route(color_df, visited, row['location2'], curr_path_length+row['length'], longest_path)
-        elif row['location2'] == city and row['location1'] not in visited:
-            single_longest_route(color_df, visited, row['location1'], curr_path_length+row['length'], longest_path)
+        if row['location1'] == city and set([city, row['location2']]) not in visited:
+            visited.append(set([city, row['location2']]))
+            single_longest_route(color_df, visited, row['location2'], curr_path_length + row['length'], longest_path)
+        elif row['location2'] == city and set([city, row['location1']]) not in visited:
+            visited.append(set([city, row['location1']]))
+            single_longest_route(color_df, visited, row['location1'], curr_path_length + row['length'], longest_path)
 
-    longest_path[0] = max(curr_path_length,longest_path[0])
+    # Update longest_path only after processing all rows for the current city
+    longest_path[0] = max(curr_path_length, longest_path[0])
 
 def longest_route(df, scores):
     longest_roads = {} 
     for key in scores.keys():
-        color_df = df[df['color']==key][['location1', 'location2', 'length']]
+        color_df = df[df['color'] == key][['location1', 'location2', 'length']]
         longest_path = 0
 
-        #iterate thru all cities bc we don't know where longest road starts
-        for city in set(color_df['location1']).union(color_df['location2']): 
-            curr_path = [0] #list to be iterable
-            single_longest_route(color_df, set(), city, 0, curr_path)
-            longest_path = max(curr_path[0],longest_path)
+        # Iterate through all cities because we don't know where the longest road starts
+        for city in set(color_df['location1']).union(color_df['location2']):
+            curr_path = [0]  # List to be iterable
+            visited = []  # List of sets to keep track of visited cities
+            single_longest_route(color_df, visited, city, 0, curr_path)
+            longest_path = max(curr_path[0], longest_path)
+
         longest_roads[key] = longest_path
 
     longest_road_color = max(longest_roads, key=longest_roads.get)
     scores[longest_road_color] += LONGEST_ROUTE_POINTS
     print("Longest road lengths: " + str(longest_roads))
     print("Longest road winner: " + longest_road_color)
+
 
 def destination_complete(color_df, curr, finish, visited):
     if curr == finish:
